4a933eedae53e2005541a5f52aa7b1de78ca6358
Patrick W. <Chelyocarpus@hotmail.com>
2024-08-02 01:08:39 +0200

    Updated for Path of Exile v3.25.0

--- Run Backup of PoB Builds.bat
+++ Run Backup of PoB Builds.bat
@@ -1,2 +1,20 @@
-xcopy "D:\Path of Building\Builds" "D:\Path-Of-Building-Builds-Backup\Builds" /s /f /d
-exit
+@echo off
+setlocal
+
+:: Define source and destination paths
+set "source=D:\Path of Building\Builds"
+set "destination=D:\Path-Of-Building-Builds-Backup\Builds"
+
+:: Remove the destination folder if it exists
+if exist "%destination%" (
+    echo Deleting existing destination folder...
+    rmdir /s /q "%destination%"
+)
+
+:: Copy the source folder to the destination
+echo Copying files...
+xcopy "%source%" "%destination%" /s /e /i /y
+
+echo Operation completed.
+endlocal
+pause
